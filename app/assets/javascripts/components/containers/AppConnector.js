72d902e68aebd04cca7bd2b903ac7fd7c1c39824
d318503 <satou-y93@mail.dnp.co.jp>
2016-04-27 10:56:50 +0900

    refactor importing mechanism of containers.

--- app/assets/javascripts/components/containers/AppConnector.js
+++ app/assets/javascripts/components/containers/AppConnector.js
@@ -4,14 +4,14 @@ import App from '../components/App'
 import * as Actions from '../actions'
 
 function mapStateToProps(state) {
-    //console.log("AppContainer mapStatetoprops(state) state=%o, state.note=%o", state, state.note)
+    //console.log("AppConnector mapStatetoprops(state) state=%o, state.note=%o", state, state.note)
     return {
 	//note: state.note
     }
 }
 
 function mapDispatchToProps(dispatch) {
-    //console.log("AppContainer mapDispatchtoprops(dispatch) Actions=%o", Actions)
+    //console.log("AppConnector mapDispatchtoprops(dispatch) Actions=%o", Actions)
     return bindActionCreators(Actions, dispatch)
 }
 
--- app/assets/javascripts/components/containers/Root.js
+++ app/assets/javascripts/components/containers/Root.js
@@ -2,22 +2,22 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 
-import AppContainer from './AppContainer'
+import AppConnector from './AppConnector'
 import configureStore from '../store/configureStore'
 
-const store = configureStore();
+const store = configureStore()
 
-export default class Root extends React.Component {
+export class Root extends Component {
 
-    componentWillMount() {
-	//console.log("Root componentWillMount() this.props.note=%o", this.props.note)
-	//store.dispatch(attachNote(this.props.note))
-    }
-    render() {
-	return (
-	    <Provider store={store}>
-	      <AppContainer />
-	    </Provider>
-	)
-    }
+  componentWillMount() {
+    //console.log("Root componentWillMount() this.props.note=%o", this.props.note)
+    //store.dispatch(attachNote(this.props.note))
+  }
+  render() {
+    return (
+      <Provider store={store}>
+	<AppConnector />
+      </Provider>
+    )
+  }
 }
