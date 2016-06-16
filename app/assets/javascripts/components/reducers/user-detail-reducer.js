068d763441c9cad70fba30dd32ed81f88ea10a33
d900650 <arai-r4@mail.dnp.co.jp>
2016-06-16 18:00:30 +0900

    added reset action changed reducer structure

--- app/assets/javascripts/components/reducers/user-detail-reducer.js
+++ app/assets/javascripts/components/reducers/user-detail-reducer.js
@@ -2,23 +2,29 @@ import * as Actions from '../actions/user-detail-action'
 
 const initialState = {
   editMode: false,
-  profile: {},
-  originalWorks: [],
+  tabKey: 1,
+  // profile: {},
+  // originalWorks: [],
   originalCount: 0,
-  favoriteWorks: [],
   favoriteCount: 0
 }
 
 export default function userDetail(state = initialState, action) {
   switch (action.type) {
-    case Actions.SET_WORKS:
+    case Actions.RESET:
+      return initialState
+    // case Actions.SET_WORKS:
+    //   return Object.assign({}, state, {
+    //     originalWorks: action.originalWorks,
+    //     favoriteWorks: action.favoriteWorks
+    //   })
+    // case Actions.SET_USER:
+    //   return Object.assign({}, state, {
+    //     profile: action.user
+    //   })
+    case Actions.CHANGE_TAB:
       return Object.assign({}, state, {
-        originalWorks: action.originalWorks,
-        favoriteWorks: action.favoriteWorks
-      })
-    case Actions.SET_USER:
-      return Object.assign({}, state, {
-        profile: action.user
+        tabKey: action.tabKey
       })
     case Actions.CHANGE_EDIT_MODE:
       return Object.assign({}, state, {
