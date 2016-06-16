77635e49ea90a055834a16a4de6726945ae0c7c7
d900650 <arai-r4@mail.dnp.co.jp>
2016-06-16 18:00:50 +0900

    no message

--- app/assets/javascripts/components/actions/user-detail-action.js
+++ app/assets/javascripts/components/actions/user-detail-action.js
@@ -1,19 +1,7 @@
-// export const SELECT_FILTER = 'SELECT_FILTER'
 export const RESET = 'UD_RESET'
-// export const SET_WORKS = 'SET_WORKS'
-// export const SET_USER = 'SET_USER'
 export const CHANGE_EDIT_MODE = 'UD_CHANGE_EDIT_MODE'
-// export const FETCH_PROFILE_REQUEST = 'FETCH_PROFILE_REQUEST'
-// export const FETCH_PROFILE_FAILURE = 'FETCH_PROFILE_FAILURE'
-// export const FETCH_PROFILE_SUCCESS = 'FETCH_PROFILE_SUCCESS'
 export const CHANGE_TAB = 'UD_CHANGE_TAB'
 
-// export function selectFilter(filter = 1) {
-//   return {
-//     type: SELECT_FILTER,
-//     selectFilter: filter
-//   }
-// }
 export function reset() {
   return {
     type: RESET,
@@ -22,23 +10,7 @@ export function reset() {
   }
 }
 
-// export function setWorks(props) {
-//   return {
-//     type: SET_WORKS,
-//     originalWorks: props.originalWorks,
-//     favoriteWorks: props.favoriteWorks
-//   }
-// }
-
-// export function setUser(user) {
-//   return {
-//     type: SET_USER,
-//     user: user
-//   }
-// }
-
 export function changeEditMode(editMode) {
-  console.log("changeEditMode")
   return {
     type: CHANGE_EDIT_MODE,
     editMode: editMode
--- app/assets/javascripts/components/reducers/user-detail-reducer.js
+++ app/assets/javascripts/components/reducers/user-detail-reducer.js
@@ -3,8 +3,6 @@ import * as Actions from '../actions/user-detail-action'
 const initialState = {
   editMode: false,
   tabKey: 1,
-  // profile: {},
-  // originalWorks: [],
   originalCount: 0,
   favoriteCount: 0
 }
@@ -13,15 +11,6 @@ export default function userDetail(state = initialState, action) {
   switch (action.type) {
     case Actions.RESET:
       return initialState
-    // case Actions.SET_WORKS:
-    //   return Object.assign({}, state, {
-    //     originalWorks: action.originalWorks,
-    //     favoriteWorks: action.favoriteWorks
-    //   })
-    // case Actions.SET_USER:
-    //   return Object.assign({}, state, {
-    //     profile: action.user
-    //   })
     case Actions.CHANGE_TAB:
       return Object.assign({}, state, {
         tabKey: action.tabKey
