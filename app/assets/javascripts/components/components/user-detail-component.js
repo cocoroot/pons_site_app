e2a479d20949b01306d98f37777969401e251a41
d900650 <arai-r4@mail.dnp.co.jp>
2016-06-17 17:22:17 +0900

    added create-work

--- app/assets/javascripts/components/components/user-detail-component.js
+++ app/assets/javascripts/components/components/user-detail-component.js
@@ -1,6 +1,4 @@
 import React, { Component, PropTypes } from 'react'
-import Header from './common-header-component'
-import Footer from './common-footer-component'
 import UserDetailProfileBox from './user-detail-profile-box-component'
 import UserDetailWorkBox from './user-detail-work-box-component'
 
