6fe80dab3f7e595cd9c629fb4135eb9f41405c13
d318503 <satou-y93@mail.dnp.co.jp>
2016-06-20 16:39:52 +0900

    change action name.

--- app/assets/javascripts/components/reducers/work-detail-reducer.js
+++ app/assets/javascripts/components/reducers/work-detail-reducer.js
@@ -1,17 +1,33 @@
 import * as Actions from '../actions/work-detail-action'
+import * as ApiActions from '../actions/core-api-action'
 import { combineReducers } from 'redux'
 
-function editMode(state = false, action) {
-  switch (action.type) {
-    case Actions.CHANGE_EDIT_MODE:
-      return action.editMode
-    default:
-      return state
+
+function editMode(state = initialState, action) {
+  if (action.type == Actions.WKD_CHANGE_EDIT_MODE) {
+    return action.editMode
+  } else {
+    return state
+  }
+}
+
+const initialStateForWork = {
+  id: 0,
+  title: '',
+  description: ''
+}
+
+function work(state = initialStateForWork, action) {
+  if (action.type == ApiActions.API_LOAD_WORK_SUCCESS) {
+    return action.payload
+  } else {
+    return state
   }
 }
 
 const workDetailReducer = combineReducers({
-  editMode
+  editMode,
+  work
 })
 
 export default workDetailReducer
