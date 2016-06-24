3286cfe9ecc1943e3591938d2da865b5cf191426
d900650 <arai-r4@mail.dnp.co.jp>
2016-06-24 14:32:24 +0900

    no message

--- app/assets/javascripts/components/components/work-detail-tags-component.js
+++ app/assets/javascripts/components/components/work-detail-tags-component.js
@@ -1,21 +1,43 @@
 import React, { Component, PropTypes } from 'react'
-import { Button, Media, Image, FormControl, FormGroup, Glyphicon, InputGroup } from 'react-bootstrap'
+import { Button, FormControl, FormGroup, Glyphicon, InputGroup } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 export default class WorkDetailTags extends Component {
 
+  onChangeInputTag(e) {
+    this.props.actions.changeInputTag(e.target.value)
+  }
+
+  onCreateWorkTag() {
+    window.alert('add tag!')
+    let id = this.props.values.currentWork.id
+    let params = {
+      name: this.props.values.tagControl.tag
+    }
+    this.props.createWorkTag(id, params)
+  }
+
+  onDeleteWorkTag() {
+    const work_tag_id = this.refs.work_tag_id.value
+    this.props.deleteWorkTag(work_tag_id)
+  }
+
   render() {
-    const editMode = true
+    // const { editMode, work_tags } = this.props.values
+    const { work_tags, tagControl } = this.props.values
+    var editMode = true
     if (!editMode) {
       return (
         <div className="workdetail-tagbox">
           <h4><Glyphicon glyph="tags"/> 登録タグ</h4>
           <p className="workdetail-tags">
-            <Button className="workdetail-tag">タグ１</Button>
-            <Button className="workdetail-tag">タグ2</Button>
-            <Button className="workdetail-tag">タグ3</Button>
-            <Button className="workdetail-tag">タグ１</Button>
-            <Button className="workdetail-tag">タグ2</Button>
-            <Button className="workdetail-tag">タグ3</Button>
+            {
+              work_tags.map((tag, index) => {
+                return (
+                  <Button className="workdetail-tag" key={index}>{tag}</Button>
+                )
+              })
+            }
           </p>
         </div>
       )
@@ -26,17 +48,21 @@ export default class WorkDetailTags extends Component {
           <div>
             <FormGroup>
               <InputGroup>
-                <FormControl type="text" />
+                <FormControl type="text" value={tagControl.tag} onChange={(e) => this.onChangeInputTag(e)} />
                 <InputGroup.Button>
-                  <Button>追加</Button>
+                  <Button disable={tagControl.updating} onClick={()=>this.onCreateWorkTag()}>追加</Button>
                 </InputGroup.Button>
               </InputGroup>
             </FormGroup>
           </div>
           <p className="workdetail-tags">
-            <Button className="workdetail-tag">タグ１<Glyphicon glyph="remove" /></Button>
-            <Button className="workdetail-tag">タグ１<Glyphicon glyph="remove" /></Button>
-            <Button className="workdetail-tag">タグ１<Glyphicon glyph="remove" /></Button>
+            {
+              work_tags.map((tag, index) => {
+                return (
+                  <Button className="workdetail-tag" key={index}>{tag}<Glyphicon glyph="remove" /></Button>
+                )
+              })
+            }
           </p>
         </div>
       )
